95e6946624abc6dfc6095c492801b71a83f791a3
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-02 18:24:28 +0300

    add set_status methods

--- code/order.cpp
+++ code/order.cpp
@@ -5,3 +5,7 @@ dimkashelk::Order::Order(const size_t from, const size_t to):
   _to(to),
   _status(ExecutionStatus::EXECUTION_CREATE)
 {}
+void dimkashelk::Order::set_status(const ExecutionStatus status)
+{
+  _status = status;
+}
