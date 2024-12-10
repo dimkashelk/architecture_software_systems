d9ce2b7791fe95d05c665f843356c08552b71798
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-10 17:18:28 +0300

    add test

--- test/test_order_stack.cpp
+++ test/test_order_stack.cpp
@@ -16,13 +16,35 @@ TEST(OrderStackTest, ADD_4_ORDER)
   order_stack.add_order(dimkashelk::Order(1, 2));
   EXPECT_EQ(order_stack.get_length(), 3) << "Different length";
 }
-TEST(OrderStackTest, TAKE_FIRST_OF_1)
+TEST(OrderStackTest, TAKE_FIRST_1)
 {
   dimkashelk::OrderStack order_stack(3);
-  dimkashelk::Order order(1, 2);
+  const dimkashelk::Order order(1, 2);
   order_stack.add_order(order);
-  auto order_from_stack = order_stack.get_first();
+  const auto order_from_stack = order_stack.get_first();
   EXPECT_EQ(order.get_id(), order_from_stack.get_id()) << "Different id";
   order_stack.remove_first();
   EXPECT_EQ(order_stack.get_length(), 0);
+}
+TEST(OrderStackTest, TAKE_FIRST_2)
+{
+  dimkashelk::OrderStack order_stack(3);
+  std::vector<dimkashelk::Order> orders(5);
+  for (int i = 0; i < 5; i++)
+  {
+    orders[i] = dimkashelk::Order(1, 2);
+    order_stack.add_order(orders[i]);
+  }
+  auto order_from_stack = order_stack.get_first();
+  EXPECT_EQ(order_from_stack.get_id(), orders[4].get_id()) << "Different id: must id of second element in stack";
+  order_stack.remove_first();
+  EXPECT_EQ(order_stack.get_length(), 2) << "Different length";
+  order_from_stack = order_stack.get_first();
+  EXPECT_EQ(order_from_stack.get_id(), orders[3].get_id()) << "Different id: must id of first element in stack";
+  order_stack.remove_first();
+  EXPECT_EQ(order_stack.get_length(), 1) << "Different length";
+  order_from_stack = order_stack.get_first();
+  EXPECT_EQ(order_from_stack.get_id(), orders[2].get_id()) << "Different id: must id of second element in stack";
+  order_stack.remove_first();
+  EXPECT_EQ(order_stack.get_length(), 0) << "Different length";
 }
