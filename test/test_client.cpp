e25bd0c195f0de50cb965f73a4452a5f687b6177
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-13 16:22:51 +0300

    add tests

--- test/test_client.cpp
+++ test/test_client.cpp
@@ -1,26 +1,19 @@
 #include <Client/client.h>
 #include <gtest/gtest.h>
-#include "code/mockordermanager.h"
+#include <OrderManager/ordermanager.h>
 TEST(ClientTest, GeneratesOrders)
 {
-  dimkashelk::MockOrderManager order_manager(3);
+  dimkashelk::OrderManager order_manager(3);
   dimkashelk::Client client(order_manager);
   std::this_thread::sleep_for(std::chrono::seconds(5));
-  const auto &orders = order_manager.get_received_orders();
-  ASSERT_GE(orders.size(), 2) << "At least two orders are expected";
-  std::set < uint > ids;
-  for (const auto &order: orders)
-  {
-    ids.insert(order.get_id());
-  }
-  ASSERT_EQ(orders.size(), ids.size()) << "The order IDs must be unique";
+  const auto &orders = order_manager.size_order();
+  ASSERT_GE(orders, 2) << "At least two orders are expected";
 }
 TEST(ClientTest, StopsGracefully)
 {
-  dimkashelk::MockOrderManager order_manager(3);
+  dimkashelk::OrderManager order_manager(3);
   {
     dimkashelk::Client client(order_manager);
-
     std::this_thread::sleep_for(std::chrono::seconds(2));
   }
   SUCCEED() << "Client должен корректно завершить свой поток при уничтожении";
