6494a659ecc08e6cf4391ea7d3fc1597d4a2ad6d
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-13 15:39:05 +0300

    add generate_order

--- code/Client/client.cpp
+++ code/Client/client.cpp
@@ -1,1 +1,15 @@
 #include "client.h"
+#include <cstdlib>
+#include <ctime>
+void dimkashelk::Client::generate_order()
+{
+  std::srand(static_cast < unsigned >(std::time(nullptr)));
+  const size_t from = static_cast < size_t >(std::rand() % 100);
+  size_t to = static_cast < size_t >(std::rand() % 100);;
+  while (to == from)
+  {
+    to = static_cast < size_t >(std::rand() % 100);
+  }
+  Order new_order(from, to);
+  orders_.emplace_back(new_order);
+}
--- code/Client/client.h
+++ code/Client/client.h
@@ -1,8 +1,6 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 #include <vector>
-#include <cstdlib>
-#include <ctime>
 #include <Order/order.h>
 
 namespace dimkashelk
@@ -10,21 +8,8 @@ namespace dimkashelk
   class Client
   {
   public:
-    Client()
-    {
-      std::srand(static_cast < unsigned >(std::time(nullptr)));
-    }
-    void generate_order()
-    {
-      const size_t from = static_cast < size_t >(std::rand() % 100);
-      size_t to = static_cast < size_t >(std::rand() % 100);;
-      while (to == from)
-      {
-        to = static_cast < size_t >(std::rand() % 100);
-      }
-      Order new_order(from, to);
-      orders_.emplace_back(new_order);
-    }
+    Client();
+    void generate_order();
     const std::vector < Order > &get_orders() const
     {
       return orders_;
