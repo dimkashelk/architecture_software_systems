8931c9d00ede9f9afb101981f669461c36cafb5d
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-13 15:42:58 +0300

    add fields

--- code/OrderManager/ordermanager.h
+++ code/OrderManager/ordermanager.h
@@ -1,6 +1,7 @@
 #ifndef ORDERMANAGER_H
 #define ORDERMANAGER_H
 #include <mutex>
+#include <thread>
 #include <Order/order.h>
 #include <OrderStack/orderstack.h>
 
@@ -16,6 +17,9 @@ namespace dimkashelk
 
   private:
     OrderStack order_stack_;
+    OrderManager &order_manager_;
+    std::thread worker_thread_;
+    std::atomic<bool> stop_flag_;
     mutable std::mutex mtx_;
   };
 }
