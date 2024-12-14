265c42bf6f6cdbbe378e52470f16566ea5885428
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-15 01:28:00 +0300

    initial commit

---
+++ code/EventManager/eventmanager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by notebook on 14.12.2024.
+//
+
+#include "eventmanager.h"
---
+++ code/EventManager/eventmanager.h
@@ -0,0 +1,12 @@
+#ifndef EVENTMANAGER_H
+#define EVENTMANAGER_H
+
+
+
+class EventManager {
+
+};
+
+
+
+#endif
