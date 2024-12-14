68b676976faa21c9eaadffd398275924a3a6287e
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-15 02:06:09 +0300

    update output

--- code/EventManager/eventmanager.cpp
+++ code/EventManager/eventmanager.cpp
@@ -30,7 +30,13 @@ void dimkashelk::EventManager::logEvent(const std::string &event)
   std::lock_guard lock(logMutex);
   if (logFile.is_open())
   {
-    logFile << event << std::endl;
+    const auto now = std::chrono::system_clock::now();
+    const auto now_time_t = std::chrono::system_clock::to_time_t(now);
+    const auto now_ms = std::chrono::duration_cast < std::chrono::milliseconds >(now.time_since_epoch()) % 1000;
+    std::ostringstream timestamp;
+    timestamp << "[" << std::put_time(std::localtime(&now_time_t), "%Y-%m-%d %H:%M:%S")
+      << "." << std::setfill('0') << std::setw(3) << now_ms.count() << "] ";
+    logFile << timestamp.str() << event << std::endl;
   }
   else
   {
