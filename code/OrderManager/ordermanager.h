467792640827692d48c76b03c23b7f55247a319b
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-14 13:28:18 +0300

    change to shared_ptr

--- code/OrderManager/ordermanager.h
+++ code/OrderManager/ordermanager.h
@@ -1,5 +1,6 @@
 #ifndef ORDERMANAGER_H
 #define ORDERMANAGER_H
+#include <memory>
 #include <mutex>
 #include <Order/order.h>
 #include <OrderStack/orderstack.h>
@@ -10,13 +11,13 @@ namespace dimkashelk
   {
   public:
     explicit OrderManager(size_t size_order);
-    void add_order(const Order &order);
+    void add_order(const Order &order) const;
     void failed_order(Order &order) const;
     void done_order(Order &order) const;
     size_t count_orders() const;
 
   private:
-    OrderStack order_stack_;
+    std::shared_ptr < OrderStack > order_stack_;
     mutable std::mutex mtx_;
   };
 }
