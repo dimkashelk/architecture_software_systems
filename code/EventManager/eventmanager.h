cdda1ac1829db6e0eff6cf1ec35dc4109958d5aa
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-15 01:33:48 +0300

    add class EventManager

--- code/EventManager/eventmanager.h
+++ code/EventManager/eventmanager.h
@@ -1,12 +1,24 @@
 #ifndef EVENTMANAGER_H
 #define EVENTMANAGER_H
+#include <string>
+#include <fstream>
+#include <mutex>
 
+namespace dimkashelk
+{
+  class EventManager
+  {
+  public:
+    static EventManager &getInstance();
+    void logEvent(const std::string &event);
 
-
-class EventManager {
-
-};
-
-
-
+  private:
+    EventManager();
+    ~EventManager();
+    EventManager(const EventManager &) = delete;
+    EventManager &operator=(const EventManager &) = delete;
+    std::ofstream logFile;
+    std::mutex logMutex;
+  };
+}
 #endif
