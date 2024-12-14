ab4713869d820c98e62e924d974d8a805337dce5
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-15 01:34:18 +0300

    create methods

--- code/EventManager/eventmanager.cpp
+++ code/EventManager/eventmanager.cpp
@@ -1,5 +1,34 @@
-//
-// Created by notebook on 14.12.2024.
-//
-
 #include "eventmanager.h"
+#include <iostream>
+dimkashelk::EventManager::EventManager()
+{
+  logFile.open("log/main.log", std::ios::app);
+  if (!logFile.is_open())
+  {
+    throw std::runtime_error("Unable to open log file");
+  }
+}
+dimkashelk::EventManager::~EventManager()
+{
+  if (logFile.is_open())
+  {
+    logFile.close();
+  }
+}
+dimkashelk::EventManager &dimkashelk::EventManager::getInstance()
+{
+  static EventManager instance;
+  return instance;
+}
+void dimkashelk::EventManager::logEvent(const std::string &event)
+{
+  std::lock_guard lock(logMutex);
+  if (logFile.is_open())
+  {
+    logFile << event << std::endl;
+  }
+  else
+  {
+    std::cerr << "Log file is not open!" << std::endl;
+  }
+}
