14570ec70feb06063a41b7d606fecf587296d6ae
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-15 17:39:49 +0300

    update logs

--- code/OrderStack/orderstack.cpp
+++ code/OrderStack/orderstack.cpp
@@ -26,7 +26,8 @@ void dimkashelk::OrderStack::add_order(const std::shared_ptr < Order > &order)
     ++count_;
   }
   stack_[index] = order;
-  EventManager::getInstance().logEvent("(OrderStack) add " + order->to_string() + " in position: " + std::to_string(index));
+  EventManager::getInstance().logEvent("(OrderStack) add " + order->to_string() +
+                                       " in position: " + std::to_string(index));
 }
 std::shared_ptr < dimkashelk::Order > &dimkashelk::OrderStack::get_first()
 {
@@ -36,6 +37,8 @@ std::shared_ptr < dimkashelk::Order > &dimkashelk::OrderStack::get_first()
     throw std::runtime_error("Stack is empty!");
   }
   const size_t last_index = (start_ + count_ - 1) % capacity_;
+  EventManager::getInstance().logEvent("(OrderStack) get " + stack_[last_index]->to_string() +
+                                       " from " + std::to_string(last_index));
   return stack_[last_index];
 }
 void dimkashelk::OrderStack::remove_first()
