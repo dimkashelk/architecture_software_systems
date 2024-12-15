4ec7c79cb7eab56812783c8b61204a93ecd6a088
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-15 17:05:41 +0300

    move get_id to cpp

--- code/Order/counter.cpp
+++ code/Order/counter.cpp
@@ -1,6 +1,12 @@
 #include "counter.h"
+
 namespace dimkashelk::details
 {
   Counter *Counter::obj_ = nullptr;
   std::mutex Counter::static_mtx_;
+  size_t Counter::get_id()
+  {
+    std::lock_guard lock(mtx_);
+    return id_++;
+  }
 }
--- code/Order/counter.h
+++ code/Order/counter.h
@@ -2,38 +2,31 @@
 #define CODER_H
 #include <mutex>
 
-namespace dimkashelk
+namespace dimkashelk::details
 {
-  namespace details
+  class Counter
   {
-    class Counter
+  public:
+    size_t get_id();
+    static Counter *get_obj()
     {
-    public:
-      size_t get_id()
+      std::lock_guard lock(static_mtx_);
+      if (obj_ == nullptr)
       {
-        std::lock_guard lock(mtx_);
-        return id_++;
+        obj_ = new Counter();
       }
-      static Counter *get_obj()
-      {
-        std::lock_guard lock(static_mtx_);
-        if (obj_ == nullptr)
-        {
-          obj_ = new Counter();
-        }
-        return obj_;
-      }
+      return obj_;
+    }
 
-    private:
-      Counter() :
-        id_(0)
-      {
-      }
-      size_t id_;
-      static Counter *obj_;
-      static std::mutex static_mtx_;
-      std::mutex mtx_;
-    };
-  }
+  private:
+    Counter() :
+      id_(0)
+    {
+    }
+    size_t id_;
+    static Counter *obj_;
+    static std::mutex static_mtx_;
+    std::mutex mtx_;
+  };
 }
 #endif
