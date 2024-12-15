76c30618d9a0966cd13b0682b7489a574dbe3d86
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-15 17:14:38 +0300

    fix test_client.cpp

--- test/test_client.cpp
+++ test/test_client.cpp
@@ -4,7 +4,7 @@
 TEST(ClientTest, GeneratesOrders)
 {
   dimkashelk::OrderManager order_manager(3);
-  dimkashelk::Client client(order_manager);
+  dimkashelk::Client client(1, order_manager);
   std::this_thread::sleep_for(std::chrono::seconds(5));
   const auto &orders = order_manager.count_orders();
   ASSERT_GE(orders, 2) << "At least two orders are expected";
@@ -13,7 +13,7 @@ TEST(ClientTest, StopsGracefully)
 {
   dimkashelk::OrderManager order_manager(3);
   {
-    dimkashelk::Client client(order_manager);
+    dimkashelk::Client client(1, order_manager);
     std::this_thread::sleep_for(std::chrono::seconds(2));
   }
   SUCCEED() << "Client должен корректно завершить свой поток при уничтожении";
