96e08430227541a3947b527b55229078607907eb
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-16 23:33:19 +0300

    link to CMake

--- code/Application/mainwindow.h
+++ code/Application/mainwindow.h
@@ -1,23 +1,23 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
 #include <QMainWindow>
-
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
+namespace Ui
 {
-    Q_OBJECT
+  class MainWindow;
+}
+
+QT_END_NAMESPACE
+class MainWindow: public QMainWindow
+{
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
