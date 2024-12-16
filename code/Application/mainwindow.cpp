c7c79cd09a3d982f7062fc4976e190872b3d7f4b
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-17 01:40:09 +0300

    update design

--- code/Application/mainwindow.cpp
+++ code/Application/mainwindow.cpp
@@ -7,8 +7,8 @@ MainWindow::MainWindow(QWidget *parent):
 {
   ui->setupUi(this);
   connect(ui->button_start_stop, &QPushButton::clicked, this, &MainWindow::start_stop);
-  connect(ui->button_clients_decrease, &QPushButton::clicked, this, &MainWindow::decrease_clients);
-  connect(ui->button_clients_increase, &QPushButton::clicked, this, &MainWindow::increase_clients);
+  connect(ui->button_clients_count_decrease, &QPushButton::clicked, this, &MainWindow::decrease_clients);
+  connect(ui->button_clients_count_increase, &QPushButton::clicked, this, &MainWindow::increase_clients);
   connect(ui->button_robots_decrease, &QPushButton::clicked, this, &MainWindow::decrease_robots);
   connect(ui->button_robots_increase, &QPushButton::clicked, this, &MainWindow::increase_robots);
   connect(ui->button_stack_decrease, &QPushButton::clicked, this, &MainWindow::decrease_stack);
