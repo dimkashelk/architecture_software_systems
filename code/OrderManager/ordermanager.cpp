01b069537894aff75006f506cf22bbb4561d6792
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-17 00:09:01 +0300

    update mutex

--- code/OrderManager/ordermanager.cpp
+++ code/OrderManager/ordermanager.cpp
@@ -13,15 +13,14 @@ void dimkashelk::OrderManager::add_order(const std::shared_ptr < Order > &order)
 }
 void dimkashelk::OrderManager::failed_order(Order &order) const
 {
-  std::lock_guard lock(mtx_);
   order.set_status(EXECUTION_FAILED);
 }
 void dimkashelk::OrderManager::done_order(Order &order) const
 {
-  std::lock_guard lock(mtx_);
   order.set_status(EXECUTION_DONE);
 }
 size_t dimkashelk::OrderManager::count_orders() const
 {
+  std::lock_guard lock(mtx_);
   return order_stack_->get_length();
 }
