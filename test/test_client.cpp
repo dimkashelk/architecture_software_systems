4db1bac93c1050c36f3dd598b7f6c4c0929b75c3
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2024-12-17 18:50:50 +0300

    update

--- test/test_client.cpp
+++ test/test_client.cpp
@@ -5,7 +5,7 @@ TEST(ClientTest, GeneratesOrders)
 {
   dimkashelk::OrderManager order_manager(3);
   dimkashelk::Client client(1, order_manager);
-  std::this_thread::sleep_for(std::chrono::seconds(5));
+  std::this_thread::sleep_for(std::chrono::seconds(8));
   const auto &orders = order_manager.count_orders();
   ASSERT_GE(orders, 2) << "At least two orders are expected";
 }
